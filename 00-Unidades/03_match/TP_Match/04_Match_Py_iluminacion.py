412199e94afedb2c6cc96c6c6143d4651de62e83
jjuannnn <juancleiva20@gmail.com>
2024-02-16 14:07:57 -0300

    TP Match terminado

--- 00-Unidades/03_match/TP_Match/04_Match_Py_iluminacion.py
+++ 00-Unidades/03_match/TP_Match/04_Match_Py_iluminacion.py
@@ -5,8 +5,8 @@ from tkinter.simpledialog import askstring as prompt
 import customtkinter
 
 '''
-nombre:
-apellido:
+nombre: Juan Cruz 
+apellido: Leiva
 ---
 TP: Iluminación
 ---
@@ -16,7 +16,7 @@ Todas las lámparas están  al mismo precio de $800 pesos final.
 		B.	Si compra 5  lamparitas bajo consumo marca "ArgentinaLuz" se hace un descuento del 40 % y si es de otra marca el descuento es del 30%.
 		C.	Si compra 4  lamparitas bajo consumo marca "ArgentinaLuz" o “FelipeLamparas” se hace un descuento del 25 % y si es de otra marca el descuento es del 20%.
 		D.	Si compra 3  lamparitas bajo consumo marca "ArgentinaLuz"  el descuento es del 15%, si es  “FelipeLamparas” se hace un descuento del 10 % y si es de otra marca un 5%.
-		E.	Si el importe final con descuento suma más de $4000  se obtien un descuento adicional de 5%.
+		E.	Si el importe final con descuento suma más de $4000  se obtiene un descuento adicional de 5%.
 '''
 
 class App(customtkinter.CTk):
@@ -35,7 +35,7 @@ class App(customtkinter.CTk):
         self.label2 = customtkinter.CTkLabel(master=self, text="Cantidad")
         self.label2.grid(row=1, column=0, padx=10, pady=10)
 
-        self.combobox_cantidad = customtkinter.CTkComboBox(master=self, values= ["1", "2","3","4","5","6","7","8","9","10","11","12"])
+        self.combobox_cantidad = customtkinter.CTkComboBox(master=self, values= ["1","2","3","4","5","6","7","8","9","10","11","12"])
         self.combobox_cantidad.grid(row=1, column=1, padx=10, pady=10)
                 
         self.btn_calcular = customtkinter.CTkButton(master=self, text="Calcular", command=self.btn_calcular_on_click)
@@ -43,9 +43,139 @@ class App(customtkinter.CTk):
 
 
     def btn_calcular_on_click(self):
-        pass
+        marca=self.combobox_marca.get()
+        cant=int(self.combobox_cantidad.get())
+        valor=800
+        total=valor*cant
+# ------- IF marca // MATCH cantidad -------
+        match cant:
+            case 6|7|8|9|10|11|12:
+                descuento= 0.50
+            
+            case 5:
+                if marca == "ArgentinaLuz":
+                    descuento= 0.40
+                else:
+                    descuento= 0.30
+
+            case 4:
+                if marca == "ArgentinaLuz" or marca=="FelipeLamparas":
+                    descuento= 0.25
+                else:
+                    descuento= 0.20
+            
+            case 3:
+                if marca == "ArgentinaLuz":
+                    descuento= 0.15
+                elif marca == "FelipeLamparas":
+                    descuento== 0.10
+                else:
+                    descuento= 0.05
+            
+            case _:
+                descuento=0
+                
+        precio= total - (total * descuento)
         
-    
+        if precio > 4000:
+            descuento_dos= total * 0.05
+        else:
+            descuento_dos= 0
+
+        precio_final= precio - descuento_dos
+
+        alert("Lamparas", f"El valor, con el descuento aplicado, es de: {precio_final}")
+
+
+'''
+------- MATCH marca // IF cantidad -------
+
+        if cant in [6,7,8,9,10,11,12]:
+            descuento= 0.50
+        elif cant == 5:
+                match marca:
+                    case "ArgentinaLuz":
+                        descuento= 0.40
+                    case _:
+                        descuento= 0.30
+        elif cant == 4:
+                match marca:
+                    case "ArgentinaLuz"|"FelipeLamparas":
+                        descuento= 0.25
+                    case _:
+                        descuento= 0.20
+        elif cant == 3:
+                match marca:
+                    case "ArgentinaLuz":
+                        descuento= 0.15
+                    case "FelipeLamparas":
+                        descuento== 0.10
+                    case _:
+                        descuento= 0.05
+        else:
+                descuento=0
+        
+        precio= total - (total * descuento)
+
+        if precio > 4000:
+            descuento_dos= total * 0.05
+        else:
+            descuento_dos= 0
+
+        precio_final= precio - descuento_dos
+
+        alert("Lamparas", f"El valor, con el descuento aplicado, es de: {precio_final}")
+'''
+
+'''
+------- MATCH marca // MATCH cantidad -------
+
+        match cant:
+            case 6|7|8|9|10|11|12:
+                descuento= 0.50
+            
+            case 5:
+                match marca:
+                    case "ArgentinaLuz":
+                        descuento= 0.40
+                    case _:
+                        descuento= 0.30
+
+            case 4:
+                match marca:
+                    case "ArgentinaLuz"|"FelipeLamparas":
+                        descuento= 0.25
+                    case _:
+                        descuento= 0.20
+            
+            case 3:
+                match marca:
+                    case "ArgentinaLuz":
+                        descuento= 0.15
+                    case "FelipeLamparas":
+                        descuento== 0.10
+                    case _:
+                        descuento= 0.05
+            
+            case _:
+                descuento=0
+
+        precio= total - (total * descuento)
+
+        if precio > 4000:
+            descuento_dos= total * 0.05
+        else:
+            descuento_dos= 0
+
+        precio_final= precio - descuento_dos
+
+        alert("Lamparas", f"El valor, con el descuento aplicado, es de: {precio_final}")
+'''
+
+
+
+
+
 if __name__ == "__main__":
     app = App()
     app.geometry("300x300")
